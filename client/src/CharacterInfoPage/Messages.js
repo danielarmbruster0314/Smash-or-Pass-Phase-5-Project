b0b5a20d289509e874507ffba7bd12505dc0e275
Daniel Armbruster <danielgundam00@gmail.com>
2022-05-26 22:16:46 -0600

    added validate buttons to the messages and changed the bacground image color for the image slider

--- client/src/CharacterInfoPage/Messages.js
+++ client/src/CharacterInfoPage/Messages.js
@@ -3,6 +3,7 @@ import {useState} from 'react';
 import {AnimateSharedLayout,AnimatePresence, motion} from 'framer-motion/dist/framer-motion'
 import RelatedComments from './RelatedComments';
 import Avatar from '@mui/material/Avatar';
+import { positions } from '@mui/system';
 
 const items = [0, 1, 2,3,4,5,6,7,8,9,10,11,12,13,14,15];
 function Messages(){
@@ -26,16 +27,46 @@ function Messages(){
           <motion.div className="messages_li" style={{}} layout onClick={()=>setIsOpen(true)} onDoubleClick={()=>setIsOpen(false)}  initial={{ borderRadius: 10 }}>
             <motion.div className="avatar" layout />
             <motion.div className='message_container'>
+            <div style={{
+              position: 'relative'
+            }}>
+            <motion.button
+            className="validate_button"
+            style={{
+              borderRadius: '10px',
+              outline: 'none',
+              position: 'absolute',
+              top: '-27px',
+              right: '155px',
+              border: 'none'
+            }}
+            
+            >🥵 Validate</motion.button>
+            <motion.button
+            style={{
+              borderRadius: '10px',
+              backgroundColor: 'green',
+              outline: 'none',
+              position: 'absolute',
+              top: '-27px',
+              right: '0px',
+              border: 'none'
+            }}
+            
+            
+            >🥶 Invalidate</motion.button>
+            </div>
             <Avatar />
             <motion.p className="message_text">hello jhfsjdfajkshfkasfksdhjfaksfkasjdf asjdfhasjkhfkasdhfj asjdlfhajksdfhkjasdhfk asjkldfhajksdhfkjsdhfk jksfhkasdhfkjsd kjhsdfkjahdkjfh  sdfhaiusdfuaisdfbas aisdfuhasidfhuasdfi aisdghfiuasdfhuiashf aisdufhiaushfuiashif asdhfuiashfuashdi aisudhfiuashfiuasdhf iasdhfiuahsdfuihasd idhfiuashdfuihd </motion.p>
             </motion.div>
+            
             <AnimatePresence>
               <motion.div>
               {isOpen ? ( 
                 <>
               <RelatedComments />
               <motion.forum>
-                <motion.input typ='textarea' placeholder="Elaborate on this"/>
+                <motion.textarea typ='textarea' placeholder="Elaborate on this topic..."/>
               </motion.forum></>): null}
               </motion.div>
               
